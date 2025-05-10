0fb00f4d5b16caa7c7b4f2801814612fbb80f593
Bruno Santana <bruno.felicio@fichacerta.com.br>
2025-05-09 22:39:58 -0300

    Alterando models de link e user

--- src/models/link.js
+++ src/models/link.js
@@ -2,31 +2,36 @@ const Sequelize = require('sequelize');
 const database = require('./../../db');
 const User = require('./user');
 
-const Link = database.define('link', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
+const Link = database.define('link',
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+    },
+    code: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    url: {
+      type: Sequelize.STRING(2083),
+      allowNull: false
+    },
+    domain: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    hits: {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+      defaultValue: 0
+    }
   },
-  code: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  url: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  domain: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  hits: {
-    type: Sequelize.INTEGER,
-    allowNull: true,
-    defaultValue: 0
+  {
+    paranoid: true
   }
-})
+)
 
 User.hasMany(Link, {
   foreignKey: 'userId'
--- src/models/user.js
+++ src/models/user.js
@@ -1,39 +1,44 @@
 const Sequelize = require('sequelize');
 const database = require('./../../db');
 
-const User = database.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
+const User = database.define('user',
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    avatar: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    profile: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    payment: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    }
   },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    unique: true
-  },
-  password: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: true
-  },
-  avatar: {
-    type: Sequelize.STRING,
-    allowNull: true
-  },
-  profile: {
-    type: Sequelize.STRING,
-    allowNull: true
-  },
-  payment: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-    defaultValue: false
+  {
+    paranoid: true
   }
-})
+)
 
 module.exports = User;
